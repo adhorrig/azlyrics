f61a7f72df3265fbe46493ee14a457cc1410b318
Adam <adhorrig@gmail.com>
2018-03-16 12:03:51 +0000

    Prepare for PyPy

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'azlyrics',
+  packages = ['azlyrics'],
+  version = '0.1',
+  description = 'An API wrapper for azlyrics which allows you to programatically extract data',
+  author = 'Adam Horrigan',
+  author_email = 'adhorrig@gmail.com',
+  url = 'https://github.com/adhorrig/azlyrics',
+  download_url = 'https://github.com/adhorrig/azlyrics/archive/0.1.tar.gz',
+  keywords = ['azlyrics', 'lyrics', 'albums', 'artists', 'songs', 'music', 'api'],
+  classifiers = [],
+)
